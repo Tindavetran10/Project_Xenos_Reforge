0c6e982d97e273ee1ec965b2952dcbcca51c0bfb
Tindavetran10 <gade.tintran@gmail.com>
2024-01-12 23:58:16 +0700

    Update Clone Skill

--- Assets/Script/Player/Skills/SkillController/CloneSkillController.cs
+++ Assets/Script/Player/Skills/SkillController/CloneSkillController.cs
@@ -4,9 +4,32 @@ namespace Script.Player.Skills.SkillController
 {
     public class CloneSkillController : MonoBehaviour
     {
+        private SpriteRenderer _spriteRenderer;
+
+        [SerializeField] private float colorTransparentSpeed;
+        [SerializeField] private float cloneDuration;
+        private float cloneTimer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            cloneTimer -= Time.deltaTime;
+
+            if (cloneTimer < 0)
+            {
+                _spriteRenderer.color = new Color(1, 1, 1,
+                    _spriteRenderer.color.a - Time.deltaTime * colorTransparentSpeed);
+            }
+        }
+
         public void SetupClone(Transform newTransform)
         {
             transform.position = newTransform.position;
+            cloneTimer = cloneDuration;
         }
     
     }
