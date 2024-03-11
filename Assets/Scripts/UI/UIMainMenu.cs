4a66a025b01734756b720f5721761575c9bc2664
khaitri1912 <85475641+khaitri1912@users.noreply.github.com>
2024-03-11 23:54:52 +0700

    fifth commit 1
    make a Graphics Setting Menu

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 namespace UI
 {
@@ -13,16 +14,63 @@ namespace UI
         [SerializeField] private GameObject continueButton;
         [SerializeField] private UIFadeScreen fadeScreen;
 
+
+
         [SerializeField] private TMP_Text volumeTextValue = null;
         [SerializeField] private Slider volumeSlider = null;
         [SerializeField] private GameObject confirmationPromt = null;
         [SerializeField] private float defaultVolume = 1.0f;
-        
+
+
+
+        [SerializeField] private Slider brightnessSlider = null;
+        [SerializeField] private TMP_Text brightnessTextValue = null;
+        [SerializeField] private float defaultBrightness = 1;
+        [SerializeField] private TMP_Dropdown qualityDropdown;
+        [SerializeField] private Toggle fullScreenToggle;
+
+        private int _qualityLevel;
+        private bool _isFullScreen;
+        private float _brightnessLevel;
+
+
+
+        public TMP_Dropdown resolutionDropdown;
+        private Resolution[] resolutions;
+
+
         private void Start()
         {
+            resolutions = Screen.resolutions;
+            resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+
+            int currentResolutionIndex = 0;
+
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + "x" + resolutions[i].height;
+                options.Add(option);
+
+                if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+                    currentResolutionIndex = i;
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+
             if(SaveManager.Instance.HasSaveData() == false)
                 continueButton.SetActive(false);
         }
+
+
+        public void SetResolution(int resolutionIndex){
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
+
 
         public void ContinueGame()
         {
@@ -63,6 +111,36 @@ namespace UI
         }
 
 
+        public void SetBrightness(float brightness){
+            _brightnessLevel = brightness;
+            brightnessTextValue.text = brightness.ToString("0.0");
+        }
+
+
+        public void SetFullScreen(bool isFullScreen){
+            _isFullScreen = isFullScreen;
+        }
+
+        public void SetQuality(int qualityIndex){
+            _qualityLevel = qualityIndex;
+        }
+
+        public void GraphicsApply(){
+            PlayerPrefs.SetFloat("masterBrightness", _brightnessLevel);
+            //change your brightness with your post processing or whatever it is
+
+
+            PlayerPrefs.SetInt("masterQuality", _qualityLevel);
+            QualitySettings.SetQualityLevel(_qualityLevel);
+
+
+            PlayerPrefs.SetInt("masterFullScreen", (_isFullScreen ? 1 : 0));
+            Screen.fullScreen = _isFullScreen;
+
+            StartCoroutine(ConfirmationBox());
+        }
+
+
         public void ResetButton(string Menutype)
         {
             if (Menutype == "Audio")
@@ -71,6 +149,22 @@ namespace UI
                 volumeSlider.value = defaultVolume;
                 volumeTextValue.text = defaultVolume.ToString("0.0");
                 VolumeApply();
+            }
+
+            if (Menutype == "Graphics"){
+                brightnessSlider.value = defaultBrightness;
+                brightnessTextValue.text = defaultBrightness.ToString("0.0");
+
+                qualityDropdown.value = 1;
+                QualitySettings.SetQualityLevel(1);
+
+                fullScreenToggle.isOn = true;
+                Screen.fullScreen = true;
+
+                Resolution currentResolution = Screen.currentResolution;
+                Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+                resolutionDropdown.value = resolutions.Length;
+                GraphicsApply();
             }
         }
 
