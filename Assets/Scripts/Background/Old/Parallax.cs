9a44fe28af2abd07a82093f8c99c393934055340
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2024-05-08 17:34:26 +0700

    Add background for main sence

--- Assets/Scripts/Background/Old/Parallax.cs
+++ Assets/Scripts/Background/Old/Parallax.cs
@@ -1,70 +1,24 @@
 using Cinemachine;
 using UnityEngine;
 
-namespace Background
+namespace Background.Old
 {
-    public class Parallax : MonoBehaviour {
+    public class Parallax : MonoBehaviour
+    {
+        private Material _mat;
+        private float _distance;
 
-        //Parallax Scroll Variables
-        public CinemachineVirtualCamera cinemachineCam;//the camera
-        public Transform subject;//the subject (usually the player character)
+        [Range(0f, 0.5f)] public float speed = 0.2f;
+        private static readonly int MainTex = Shader.PropertyToID("_MainTex");
 
-
-        //Instance variables
-        private float _zPosition;
-        private Vector2 _startPos;
-
-
-        //Properties
-        private float TwoAspect => cinemachineCam.m_Lens.Aspect * 2;
-        private float TileWidth => TwoAspect > 3 ? TwoAspect : 3;
-        private float ViewWidth => loopSpriteRenderer.sprite.rect.width / loopSpriteRenderer.sprite.pixelsPerUnit;
-        private Vector2 Travel => (Vector2)cinemachineCam.transform.position - _startPos; //2D distance travelled from our starting position
-        private float DistanceFromSubject => transform.position.z - subject.position.z;
-        private float ClippingPlane => cinemachineCam.transform.position.z + (DistanceFromSubject > 0 
-            ? cinemachineCam.m_Lens.FarClipPlane : cinemachineCam.m_Lens.NearClipPlane);
-        private float ParallaxFactor => Mathf.Abs(DistanceFromSubject) / ClippingPlane;
-
-
-        //User options
-        public bool xAxis = true; //parallax on x?
-        public bool yAxis = true; //parallax on y?
-        public bool infiniteLoop; //are we looping?
-
-
-        //Loop requirement
-        public SpriteRenderer loopSpriteRenderer;
-
-
-        // Start is called before the first frame update
-        private void Awake()
+        private void Start() => _mat = GetComponent<Renderer>().material;
+        
+        private void Update()
         {
-            var position = transform.position;
-            _startPos = position;
-            _zPosition = position.z;
-
-            if (loopSpriteRenderer != null && infiniteLoop) {
-                var sprite = loopSpriteRenderer.sprite;
-                var spriteSizeX = sprite.rect.width / sprite.pixelsPerUnit;
-                var spriteSizeY = sprite.rect.height / sprite.pixelsPerUnit;
-
-                loopSpriteRenderer.drawMode = SpriteDrawMode.Tiled;
-                loopSpriteRenderer.size = new Vector2(spriteSizeX * TileWidth, spriteSizeY);
-                transform.localScale = Vector3.one;
-            }
-        }
-
-
-        // Update is called once per frame
-        private void Update() {
-            var newPos = _startPos + Travel * ParallaxFactor;
-            transform.position = new Vector3(xAxis ? newPos.x : _startPos.x, yAxis ? newPos.y : _startPos.y, _zPosition);
-
-            if (infiniteLoop) {
-                Vector2 totalTravel = cinemachineCam.transform.position - transform.position;
-                var boundsOffset = ViewWidth / 2 * (totalTravel.x > 0 ? 1 : -1);
-                float screens = (int)((totalTravel.x + boundsOffset) / ViewWidth);
-                transform.position += new Vector3(screens * ViewWidth, 0);
+            if (Camera.main != null)
+            {
+                _distance += Time.deltaTime * speed;
+                _mat.SetTextureOffset(MainTex, Vector2.right * _distance);
             }
         }
     }
